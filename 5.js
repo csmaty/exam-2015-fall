6775ef887b14516dc3793bfe272a7b3b8f095629
Matyas Csiki <matyas.csiki@gmail.com>
2016-01-22 11:34:51 +0100

    5 completed

--- 5.js
+++ 5.js
@@ -7,12 +7,6 @@
 // It should have a "deleteCarByType" method that removes the cars from the inner list
 // that have the given type
 
-
-
-
-
-
-
 var cars = [
   {type: 'Dodge', price: 20000, year: 2012},
   {type: 'Tesla', price: 30000, year: 2015},
@@ -21,11 +15,57 @@ var cars = [
   {type: 'Ferrari', price: 40000, year: 2001}
 ];
 
+
+function CarStore(cars) {
+	this.cars = cars;
+
+	this.addCar = function(type, price, year) {
+		var newCar = {};
+		newCar['type'] = type;
+		newCar['price'] = price;
+		newCar['year'] = year;
+		this.cars.push(newCar);
+	}
+
+	this.getSumPrice = function() {
+		var sumPrice = 0;
+		this.cars.forEach(function(car) {
+    	sumPrice += car.price
+   });
+  return sumPrice;
+	}
+
+	this.getOldestCarType = function() {
+		var oldestCar = this.cars[0];
+		this.cars.forEach(function(car) {
+    	if (car.year < oldestCar.year) {
+    		oldestCar = car;
+    	}
+   });
+  return oldestCar.type;
+	}
+
+	this.deleteCarByType = function(type) {
+		var index = 0
+		this.cars.forEach(function(car) {
+			if (car.type === type) {
+				cars.splice(index, 1);
+			}
+			index++
+   });
+  	return this.cars;
+	}
+
+}
+
 var carStore = new CarStore(cars);
 
 carStore.addCar('Smart', 18000, 2011);
 console.log(carStore.getSumPrice()); // 122000 
-console.log(carStore.getOldestCarType()); // 'Trabant'
+// console.log(carStore.C); // 'Trabant'
 carStore.deleteCarByType('Ferrari');
-console.log(carStore.getSumPrice()); // 82000 
+// console.log(carStore.getSumPrice()); // 82000 
 
+
+console.log(carStore.getOldestCarType());
+console.log(carStore.cars);
